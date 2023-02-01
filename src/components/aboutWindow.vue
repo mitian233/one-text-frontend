dbb82f06f6445b6e5281bad76d17e464da920b99
mitian233 <2304254165@qq.com>
2023-02-01 18:44:28 +0800

    Update aboutWindow.vue

--- src/components/aboutWindow.vue
+++ src/components/aboutWindow.vue
@@ -6,7 +6,10 @@
         <p>API 地址：<a href="https://api.onetext.project.ac.cn" >https://api.onetext.project.ac.cn</a></p>
         <p>使用 & 贡献：<a href="https://github.com/lz233/OneText-Library" >https://github.com/lz233/OneText-Library</a></p>
       </div>
-      <button style="float: right;margin-bottom: 20px" class="btn" @click="closeWindow()">关闭</button>
+      <div>
+        <span>Deploy status: </span><a href="https://app.netlify.com/sites/one-text-frontend/deploys"><img src="https://api.netlify.com/api/v1/badges/f64c3f64-8687-4850-b58c-7cbfec8663d2/deploy-status"/></a>
+        <button style="float: right;margin-bottom: 20px" class="btn" @click="closeWindow()">关闭</button>
+      </div>
     </div>
   </div>
 </div>
